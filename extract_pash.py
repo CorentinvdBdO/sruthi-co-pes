bb5c0aa4d9c744e8d5fc1b6dce76335eab8836c5
Corentin vdBdO <cvdbdo@hotmail.fr>
2021-03-19 13:34:48 +0100

    Pash to Dataframe converter

---
+++ extract_pash.py
@@ -0,0 +1,34 @@
+"""
+takes the pash.dat file and turns it into manageable data
+"""
+import numpy as np
+import pandas as pd
+import re
+
+def pash_to_dataframe():
+    """
+    Reads the file pash.dat and returns a pandas Dataframe
+    :return:
+    """
+    f = open("barrier/pash.dat", "r")
+    lines = f.readlines()
+    f.close()
+
+    columns = lines[9].strip(" \n").split("  ")
+    data = pd.DataFrame(columns = columns)
+
+    for line in lines[10:]:
+        line = line.strip(" \n")
+        line = re.split(r"\s{1,}",line)
+        line = np.array(line).astype(float)
+        line = pd.DataFrame([line],columns = columns)
+        data = data.append(line)
+
+    return data
+
+
+
+if __name__ == "__main__":
+    print (pash_to_dataframe()["P(2)"])
+    print ("runned")
+
