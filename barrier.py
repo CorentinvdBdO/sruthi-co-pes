9bbca1269052ea2b7075222c6876defa3474e439
Sruthi Ravikularaman <sruthiranjani.ravikularaman@universite-paris-saclay.fr>
2021-03-19 17:26:59 +0100

    modified plot_surface fucntion to return x, y, z to be plotted

--- barrier.py
+++ barrier.py
@@ -3,15 +3,20 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import re
 import extract_pash
+from scipy.interpolate import griddata
 
 f = open("barrier/pash.dat", "r")
 lines = f.readlines()
 f.close()
 pash_data = extract_pash.pash_to_dataframe(lines)
+print(pash_data[' Barrier'])
 
+x, y, z = extract_pash.plot_surface(pash_data, "P(1)", "P(2)", " Barrier")
 fig = plt.figure()
 ax = fig.gca(projection='3d')
-plt.plot(pash_data["P(1)"], pash_data["P(2)"], pash_data["Barrier"])
+surf = ax.plot_surface(x, y, z, rstride=1, cstride=1, linewidth=0, antialiased=False)
+
 plt.show()
+
 
 
--- extract_pash.py
+++ extract_pash.py
@@ -23,15 +23,20 @@ def pash_to_dataframe(lines):
 
     return data
 
-def plot_surface (data, x, y, z):
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-    plt.plot(data[x], data[y], data[z])
-    plt.show()
+def plot_surface(data, key1, key2, key3):
+    """
+        Takes the DataFrame file with the keys of interest to give x, y and z to be plotted
+    """
+    x = np.linspace(data['key1'].min(), data['key1'].max(), len(data['key1'].unique()))
+    y = np.linspace(data['key2'].min(), data['key2'].max(), len(data['key2'].unique()))
+    x, y = np.meshgrid(x1, y1)
+    z = griddata((data['key1'], data['key2']), data['key3'], (x, y), method='cubic')
+
+    return x, y, z
 
 if __name__ == "__main__":
     data = pash_to_dataframe()
     print (data)
-    plot_surface(data, "P(1)", "P(2)", "Barrier")
+    plot_surface(data, "P(1)", "P(2)", " Barrier")
     print ("runned")
 
