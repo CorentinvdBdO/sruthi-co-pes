86f61b8a28551d103e91802badd31a3cee749f15
Corentin vdBdO <cvdbdo@hotmail.fr>
2021-03-19 16:20:38 +0100

    launch_barrier.py: input_template, launch_barrier, change_file_name

--- launch_barrier.py
+++ launch_barrier.py
@@ -3,7 +3,19 @@ Test file for Corentin when running random bits of code
 """
 import subprocess
 import os
+import shutil
 
+def input_change ():
+    """
+    Change the parameters from barrier.inp
+    """
+
+def input_template(template_name):
+    """
+    Change barrier.inp to a template
+    """
+    source = "barrier/input_templates/"+template_name+".inp"
+    shutil.copy(source, "barrier/barrier.inp")
 
 
 def launch_barrier ():
@@ -18,6 +30,7 @@ def launch_barrier ():
 def change_file_name (name, new_name, in_barrier = True):
     """
     Change file name into new_name
+    Inside the barrier directory by default
     """
     if in_barrier:
         os.chdir("barrier")
@@ -27,6 +40,7 @@ def change_file_name (name, new_name, in_barrier = True):
     return 0
 
 if __name__ == "__main__":
+    input_template("type2")
     launch_barrier()
     print ("runned")
 
