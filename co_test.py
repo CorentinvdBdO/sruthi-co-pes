f7eaa7846455e1ed25a7f04a34de3103ec7d2375
Corentin vdBdO <cvdbdo@hotmail.fr>
2021-03-19 11:39:23 +0100

    The barrier.exe file and its dependencies

---
+++ co_test.py
@@ -0,0 +1,12 @@
+"""
+Test file for Corentin when running random bits of code
+"""
+import subprocess
+import os
+
+if __name__ == "__main__":
+    subprocess.call("barrier/barrier.exe")
+    # barrier/barrier.exe
+    print ("runned")
+
+
