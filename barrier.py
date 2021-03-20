b01153a55178dbc59aed609b16f0e05dd2bff53e
Sruthi Ravikularaman <sruthiranjani.ravikularaman@universite-paris-saclay.fr>
2021-03-20 12:34:39 +0100

    changed plot_surface so it returns nothing and fig ready to be plotted

--- barrier.py
+++ barrier.py
@@ -8,13 +8,9 @@ from scipy.interpolate import griddata
 
 launch_barrier.input_template("step3")
 launch_barrier.launch_barrier()
-launch_barrier.change_file_name("pash.dat", "pash_step3.dat")
-pash_data = extract_pash.pash_to_dataframe("barrier/pash_step3.dat")
-
-x, y, z = extract_pash.plot_surface(pash_data, "P(1)", "P(2)", "Barrier")
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-surf = ax.plot_surface(x, y, z, rstride=1, cstride=1, linewidth=0, antialiased=False)
+launch_barrier.change_file_name("pash.dat", "pash_step3new.dat")
+pash_data = extract_pash.pash_to_dataframe("barrier/pash_step3new.dat")
+extract_pash.plot_surface(pash_data, "P(1)", "P(2)", "Barrier")
 
 plt.show()
 
--- extract_pash.py
+++ extract_pash.py
@@ -3,6 +3,7 @@ takes the pash.dat file and turns it into manageable data
 """
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import griddata
 import pandas as pd
 import re
 
@@ -38,16 +39,18 @@ def plot_surface(data, key_1, key_2, key_3):
     """
         Takes the DataFrame file with the keys of interest to give x, y and z to be plotted
     """
-    x = np.linspace(data[key_1].min(), data[key_1].max(), len(data[key_1].unique()))
-    y = np.linspace(data[key_2].min(), data[key_2].max(), len(data[key_2].unique()))
+    x1 = np.linspace(data[key_1].min(), data[key_1].max(), len(data[key_1].unique()))
+    y1 = np.linspace(data[key_2].min(), data[key_2].max(), len(data[key_2].unique()))
     x, y = np.meshgrid(x1, y1)
     z = griddata((data[key_1], data[key_2]), data[key_3], (x, y), method='cubic')
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    surf = ax.plot_surface(x, y, z, rstride=1, cstride=1, linewidth=0, antialiased=False)
 
-    return x, y, z
 
 if __name__ == "__main__":
-    data = pash_to_dataframe()
-    print (data["P(1)", "P(2)"])
-    plot_surface(data, "P(1)", "P(2)", " Barrier")
+    data = pash_to_dataframe("barrier/pash_step3.dat")
+    print (data.columns)
+    plot_surface(data, "P(1)", "P(2)", "Barrier")
     print ("ran")
 
