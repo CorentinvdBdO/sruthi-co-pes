fd81d3550fa5502aea326e89634807731b7bdaba
Sruthi Ravikularaman <sruthiranjani.ravikularaman@universite-paris-saclay.fr>
2021-03-24 16:21:21 +0100

    tests for optimizers

--- hyper_test.py
+++ hyper_test.py
@@ -41,10 +41,13 @@ loss_train_epoch, loss_test_epoch, loss_train_hp, loss_test_hp = hyper_analysis(
     n_layers=3,
     n_epochs=1500,
     frac=0.1,
-    loss=['mean_squared_error', 'mean_absolute_error', 'cosine_similarity', 'huber', 'logcosh'])
+    optimizer=['RMSprop', 'Adam', 'Adamax', 'Nadam'])
 
-los=[1,2,3,4,5]
-loss=['mean_squared_error', 'mean_absolute_error', 'cosine_similarity', 'huber', 'logcosh']
+opt=[1,2,3,4]
+optimizer=['RMSprop', 'Adam', 'Adamax', 'Nadam']
+
+# los=[1,2,3,4,5]
+# loss=['mean_squared_error', 'mean_absolute_error', 'cosine_similarity', 'huber', 'logcosh']
 
 # plt.plot(opt, loss_train_hp, 'g+')
 # plt.plot(opt, loss_test_hp, 'b+')
@@ -52,34 +55,31 @@ loss=['mean_squared_error', 'mean_absolute_error', 'cosine_similarity', 'huber',
 n_epochs = 1500
 epochs1=np.arange(1,n_epochs+1,1)
 epochs2=np.arange(1,n_epochs+1,10)
-fig, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2,3)
-fig.suptitle("Learning curves for different loss functions")
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2,2)
+fig.suptitle("Learning curves for different optimizers")
 plt.yscale("log")
 
 ax1.semilogy(epochs1, loss_train_epoch[0],'g+')
 ax1.semilogy(epochs2, loss_test_epoch[0],'b+')
 ax1.set_ylabel("Loss")
-ax1.set_title("mean_squared_error")
+ax1.set_title("RMSprop")
 
 ax2.semilogy(epochs1, loss_train_epoch[1],'g+')
 ax2.semilogy(epochs2, loss_test_epoch[1],'b+')
 ax2.set_ylabel("Loss")
-ax2.set_title("mean_absolute_error")
+ax2.set_title("Adam")
 
 ax3.semilogy(epochs1, loss_train_epoch[2],'g+')
 ax3.semilogy(epochs2, loss_test_epoch[2],'b+')
+ax3.set_xlabel("Number of epochs")
 ax3.set_ylabel("Loss")
-ax3.set_title("cosine_similarity")
+ax3.set_title("Adamax")
 
 ax4.semilogy(epochs1, loss_train_epoch[3],'g+')
 ax4.semilogy(epochs2, loss_test_epoch[3],'b+')
+ax4.set_xlabel("Number of epochs")
 ax4.set_ylabel("Loss")
-ax4.set_title("huber")
-
-ax5.semilogy(epochs1, loss_train_epoch[4],'g+')
-ax5.semilogy(epochs2, loss_test_epoch[4],'b+')
-ax5.set_ylabel("Loss")
-ax5.set_title("logcosh")
+ax4.set_title("Nadam")
 
 plt.show()
 
