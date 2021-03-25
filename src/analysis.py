dd7fd46ec2e039ddd8cbb45fcd8b77b849881b9f
Sruthi Ravikularaman <sruthiranjani.ravikularaman@universite-paris-saclay.fr>
2021-03-25 23:01:53 +0100

    added doc strings

--- src/analysis.py
+++ src/analysis.py
@@ -5,17 +5,18 @@ from launch_barrier import pash_to_dataframe
 import tensorflow as tf
 from nn_regression import retransform
 
+
 def plot_surface(data, key_1, key_2, key_3, ax = None, alpha = 1):
     """
-    Takes the DataFrame with the keys of interest to be plotted on a 3D surface
-    The input data should cover the whole surface (i.e. avoid holes)
-    :param data: a DataFrame
-    :param key_1: x axis' key of the DataFrame
-    :param key_2: y axis' key of the DataFrame
-    :param key_3: z axis' key of the DataFrame
-    :param ax: if given, figure will be plotted on it
-    :param alpha: transparency of the surface
-    :return: Nothing
+    Takes the DataFrame with the keys of interest to be plotted on a 3D surface.
+    The input data should cover the whole surface (i.e. avoid holes).
+    :param data: (pandas DataFrame) initial DataFrame
+    :param key_1: (str) x axis' key of the DataFrame
+    :param key_2: (str) y axis' key of the DataFrame
+    :param key_3: (str) z axis' key of the DataFrame
+    :param ax: (axes) if given, figure will be plotted on it
+    :param alpha: (float) transparency of the surface
+    :return: None
     """
     x1 = np.linspace(data[key_1].min(), data[key_1].max(), len(data[key_1].unique()))
     y1 = np.linspace(data[key_2].min(), data[key_2].max(), len(data[key_2].unique()))
@@ -29,15 +30,16 @@ def plot_surface(data, key_1, key_2, key_3, ax = None, alpha = 1):
     ax.set_ylabel(key_2)
     ax.set_zlabel(key_3)
 
-def plot_surface_diff(data1,data2, key1, key2, key3, ax = None):
+def plot_surface_diff(data1, data2, key1, key2, key3, ax = None):
     """
-    Plot the surface difference between key3 of dataframes 1 & 2
-    :param data1: dataframe
-    :param data2: dataframe
-    :param key1: x axis
-    :param key2: y axis
-    :param key3: z axis of the graph, on which the difference is made
+    Takes two DataFrames with the keys of interest and plots the difference between key3 of DataFrames 1 & 2.
+    :param data1: (pandas DataFrame) DataFrame 1
+    :param data2: (pandas DataFrame) DataFrame 2
+    :param key1: (str) x axis' key of the DataFrame
+    :param key2: (str) y axis' key of the DataFrame
+    :param key3: (str) z axis' key of the DataFrame, on which the difference is made
     :param ax: on which the surface is plotted
+    :return: None
     """
     diff = data1[key3] - data2[key3]
     data_diff = retransform(data1[[key1, key2]], diff)
@@ -46,13 +48,17 @@ def plot_surface_diff(data1,data2, key1, key2, key3, ax = None):
 
 def plot_contour(data, key_1, key_2, key_3, levels=6, ax=plt.gca(), cmap="hot", colorbar=True, bar_name=None):
     """
-    Plot a contour graph of the input data
-    :param data: a DataFrame
-    :param key_1: x axis' key of the DataFrame
-    :param key_2: y axis' key of the DataFrame
-    :param key_3: z axis' key of the DataFrame
-    :param ax: if given, figure will be plotted on it
-    :return:
+    Takes the DataFrame with the keys of interest and plots a contour graph of the input data
+    :param data: (pandas DataFrame) DataFrame 1
+    :param key_1: (str) x axis' key of the DataFrame
+    :param key_2: (str) y axis' key of the DataFrame
+    :param key_3: (str) z axis' key of the DataFrame
+    :param levels: (int) number contour levels - 6 by default
+    :param ax: (axes) if given, figure will be plotted on it
+    :param cmap: (str) choice of colormap - 'hot' by default
+    :param colorbar: (bool) if True displays colorbar
+    :param bar_name: (str) name to be displayed next to colorbar
+    :return: None
     """
     x = np.linspace(data[key_1].min(), data[key_1].max(), len(data[key_1].unique()))
     y = np.linspace(data[key_2].min(), data[key_2].max(), len(data[key_2].unique()))
@@ -68,15 +74,21 @@ def plot_contour(data, key_1, key_2, key_3, levels=6, ax=plt.gca(), cmap="hot", 
         plt.colorbar(img, label=bar_name, ax=ax)
 
 def plot_points(data, features, ax=plt.gca()):
-
+    """
+    Takes the DataFrame with the keys of features used to place the points of training datasets
+    :param data: (pandas DataFrame) DataFrame 1
+    :param features: (str list) list of keys of features
+    :param ax: (axes) if given, figure will be plotted on it
+    :return: None
+    """
     ax.plot(data[features[0]], data[features[1]], '.')
 
 def calculate_mse(predicted_values, real_values):
     """
-    Takes dataframes to calculate root mean squared error
-    :param predicted_values: dataframe
-    :param real_values: dataframe
-    :return: float
+    Takes DataFrames to calculate the mean squared error between them.
+    :param predicted_values: (pandas DataFrame) estimated values
+    :param real_values: (pandas DataFrame) expected values
+    :return mse: (float) mean squared error
     """
     no_points = len(predicted_values)
     error = predicted_values - real_values
